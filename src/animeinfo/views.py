fefb11954558111addbcd57d25a50c9c8db48013
DeathSpawn <adikesavan28@gmail.com>
2020-09-19 17:37:37 +0530

    Added first few anime and tested site viewing

--- src/animeinfo/views.py
+++ src/animeinfo/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from .models import AnimeSeries
 
 # Create your views here.
 
 
 def index(request):
+    all_series = AnimeSeries.objects.all()
+    context = {
+        'anime': all_series
+    }
     template = loader.get_template('animeinfo/index.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render(context, request))
