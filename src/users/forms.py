e9957158a39ca9f4f030f52871161503b346aa0e
DeathSpawn <adikesavan28@gmail.com>
2020-09-20 17:10:30 +0530

    Added login page and switched a few things around

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -9,3 +9,8 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        fields = '__all__'
