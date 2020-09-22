10de2e44b4fd4b5247fc6921c989f65db11ed76b
DeathSpawn <adikesavan28@gmail.com>
2020-09-22 16:02:19 +0530

    New style, tweaked with the Bootstrap stuff to end up here.

--- src/project/settings.py
+++ src/project/settings.py
@@ -121,5 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static/"
+STATIC_DIRS = BASE_DIR / "static/"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
